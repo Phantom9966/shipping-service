4bd8f02f80f83c97c421d02221d6729eae367240
Giovambattista de Luca Carignani <giamba@MacBook-Pro-di-Giovambattista.local>
2019-06-26 18:21:34 +0200

    3

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -3,8 +3,8 @@ var productService = require('../services/product-service')
 class ShippingController {
     constructor() 
     {
-      this.REGULAR_PRICE = 0.5
-      this.OVERNIGHT_PRICE = 5
+      this.REGULAR_PRICE = 0.1
+      this.OVERNIGHT_PRICE = 1
     }
     async getItemShipping(item) {
       var shippingAmount = await productService.getProductWeight(item.id)
